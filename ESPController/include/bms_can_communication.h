cf33cd90ae209d887d0d1252f184c56269f2fb95
Goran <gorance@live.de>
2025-01-26 01:25:11 +0100

    Create bms_can_communication.h
    Added Header File for BMS CAN Communication Module
    
    Details:
    
    Created bms_can_communication.h header file to define the interface for CAN communication functionality.
    Functions included:
    setupCANCommunication(): Initializes the CAN communication hardware and configuration.
    processIncomingCANMessages(): Handles incoming messages from the CAN bus.
    Provides a clear separation between implementation and interface to improve code modularity and maintainability.
    Benefits:
    
    Facilitates cleaner and more organized project structure.
    Simplifies future enhancements and debugging of the CAN communication module.
    Ensures compatibility and consistency across files that use CAN communication.

---
+++ ESPController/include/bms_can_communication.h
@@ -0,0 +1,11 @@
+/*
+(c)2025 Trajilovic Goran
+www.globcast.eu gorance@live.de
+*/
+#ifndef BMS_CAN_COMMUNICATION_H
+#define BMS_CAN_COMMUNICATION_H
+
+void setupCANCommunication();
+void processIncomingCANMessages();
+
+#endif
